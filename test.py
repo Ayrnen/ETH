f7bc7697187eff894b1a98df3e038d4b288f82a7
Ayrnen <Ayrnen@pm.me>
2025-01-17 11:55:13 -0500

    Working on slot0 issue

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from classes.unisep_api import UniSepAPIHandler
+from classes.web3_api import Web3APIHandler
 from classes.runtime_tracker import RuntimeTracker
 from classes.config_reader import ConfigReader
 
@@ -11,11 +11,30 @@ class Placeholder:
 
 if __name__ == '__main__':
     RuntimeTracker.start()
-    web3_api = UniSepAPIHandler()
+    web3_api = Web3APIHandler()
     config = ConfigReader()
 
-    pool_address = config.get_section('uniswap-pools-sepolia')['weth-usdc']
-    print(web3_api.get_pool_tokens(pool_address))
 
+    factory_address = config.get_section('uniswap-factories')['v3']
+    factory_contract = web3_api.get_factory_contract(factory_address)
+
+    
+    pool_fee = int(config.get_section('uniswap-pools')['usdc-weth-fee'])
+    print(web3_api.get_pool_address(factory_contract, web3_api.token_addresses['usdc'], web3_api.token_addresses['weth'], 3000))
+
+    pool_fee = int(config.get_section('uniswap-pools')['usdt-weth-fee'])
+    print(web3_api.get_pool_address(factory_contract, web3_api.token_addresses['usdt'], web3_api.token_addresses['weth'], 5000))
+
+    pool_address = config.get_section('uniswap-pools')['usdc-weth']
+    pool_contract = web3_api.get_pool_contract(pool_address)
+    web3_api.get_pool_slot0(pool_contract, pool_address)
+    # print(web3_api.get_pool_tokens(pool_address))
+
+    web3_api.get_pool_slot0_old(pool_address)
+    
+    # pool_address = config.get_section('uniswap-pools')['usdt-weth']
+    # print(web3_api.get_pool_tokens(pool_address))
+
+    # web3_api.get_pool_slot0(factory_contract, pool_address)
 
     RuntimeTracker.stop()
