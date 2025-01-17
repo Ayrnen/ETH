339bfd2758e0f183490e329c0b7b9800747540a8
Ayrnen <Ayrnen@pm.me>
2025-01-17 12:53:39 -0500

    adhoc/temp file used to validate pool existence/data

---
+++ pool_validator_temp.py
@@ -0,0 +1,41 @@
+from classes.web3_api import Web3APIHandler
+from classes.runtime_tracker import RuntimeTracker
+from classes.config_reader import ConfigReader
+
+
+class PoolValidator:
+    def __init__(self):
+        self.web3_api = Web3APIHandler()
+        self.config = ConfigReader()
+        self.factory_contract = self._get_factory_contract()
+
+    def _get_factory_contract(self):
+        factory_address = self.config.get_section('uniswap-factories')['v3']
+        factory_contract = self.web3_api.get_factory_contract(factory_address)
+        return factory_contract
+    
+    def validate_pool_existence(self, token0, token1, fee_tier):
+        token0_address = self.web3_api.token_addresses[token0]
+        token1_address = self.web3_api.token_addresses[token1]
+        pool_address = self.web3_api.get_pool_address(self.factory_contract, token0_address, token1_address, fee_tier)
+        if pool_address == '0x0000000000000000000000000000000000000000':
+            return False
+        return True
+    
+    def validate_pool_metadata(self, token0, token1, fee_tier):
+        token0_address = self.web3_api.token_addresses[token0]
+        token1_address = self.web3_api.token_addresses[token1]
+        pool_address = self.web3_api.get_pool_address(self.factory_contract, token0_address, token1_address, fee_tier)
+        pool_contract = self.web3_api.get_pool_contract(pool_address)
+        slot0 = self.web3_api.get_pool_slot0(pool_contract)
+        return slot0
+
+
+if __name__ == '__main__':
+    RuntimeTracker.start()
+    pool_validator = PoolValidator()
+
+    print(pool_validator.validate_pool_existence('usdc', 'weth', 3000))
+    print(pool_validator.validate_pool_existence('usdt', 'weth', 3000))
+
+    RuntimeTracker.stop()
