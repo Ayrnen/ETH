0de0009e5b2ba54a60907fc3d237969e126a3f96
Ayrnen <Ayrnen@pm.me>
2025-01-17 12:54:18 -0500

    completed pool validation api calls

--- classes/web3_api.py
+++ classes/web3_api.py
@@ -51,7 +51,6 @@ class Web3APIHandler:
         pool_contract = self.web3.eth.contract(address=Web3.to_checksum_address(pool_address), abi=pool_abi)
         token0 = pool_contract.functions.token0().call()
         token1 = pool_contract.functions.token1().call()
-
         return {'token0': token0, 'token1': token1}
 
     def create_pool(self, factory_contract, token0, token1, fee, tick_lower, tick_upper):
@@ -60,29 +59,29 @@ class Web3APIHandler:
         pool_address = factory_contract.functions.createPool(token0_address, token1_address, fee, tick_lower, tick_upper).call()
         return pool_address
 
-
     def get_pool_contract(self, pool_address):
         pool_abi = self.etherscan.get_contract_abi(pool_address)
         pool_contract = self.web3.eth.contract(address=Web3.to_checksum_address(pool_address), abi=pool_abi)
         return pool_contract
 
+    def get_pool_slot0(self, pool_contract):
+        slot0 = pool_contract.functions.slot0().call()
 
-    def get_pool_slot0(self, pool_contract, pool_address):
-        print(pool_contract.functions.slot0().call())
+        sqrt_price_x96 = slot0[0]
+        current_tick = slot0[1]
+        observation_index = slot0[2]
+        observation_cardinality = slot0[3]
+        observation_cardinality_next = slot0[4]
+        fee_protocol = slot0[5]
+        unlocked = slot0[6]
+        price = (sqrt_price_x96 / (2 ** 96)) ** 2
 
-    def get_pool_slot0_old(self, s):
-            pool_abi = self.etherscan.get_contract_abi(pool_address)
-            pool_contract = self.web3.eth.contract(address=Web3.to_ch ecksum_address(pool_address), abi=pool_abi)
-
-            slot0 = pool_contract.functions.slot0().call()
-            
-            return {
-                'price': (slot0[0] / (2 ** 96)) ** 2,
-                'tick_index': slot0[1],
-                'obs_idx': slot0[2],
-                'obs_cardinality': slot0[3],
-                'obs_cardinality_next': slot0[4],
-                'fee_protocol': slot0[5],
-                'lock_status': slot0[6]
-            }
-        
+        return {
+            'price': price,
+            'current_tick': current_tick,
+            'observation_index': observation_index,
+            'observation_cardinality': observation_cardinality,
+            'observation_cardinality_next': observation_cardinality_next,
+            'fee_protocol': fee_protocol,
+            'unlocked': unlocked,
+        }
